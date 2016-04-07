3d26a5c1b853e35147e494e88a5cec835cc97a3c
mshaf <michael.shafland@gmail.com>
2016-04-07 00:27:47 -0400

    added CLE joke to results

--- javascripts/app.js
+++ javascripts/app.js
@@ -11,6 +11,11 @@ $(document).ready(function(){
                if(data.Response != "False"){
                	$("#movie-title").text("Title: "+data.Title);
                	$("#plot").text("Plot: "+data.Plot);
+               	$("#year").text("Year: "+data.Year);
+               	var runNum= parseInt(data.Runtime)
+               	var CLEmins=Math.round(runNum/50);
+               	$("#mins").text("Number of New York CLE credits you could receive if you watch a Lawline.com CLE Course instead of this movie: "+CLEmins);
+               	$("#director").text("Director: "+data.Director);
                	$("#actors").text("Actors: "+data.Actors);
                	$("#tomato-review").text("Rotten Tomatoes: "+data.tomatoConsensus);
                	$(".poster").html('<img src='+data.Poster+'">');
