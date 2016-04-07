6103cf2c2ac91ea076780e3e063a0812dee98dd0
mshaf <michael.shafland@gmail.com>
2016-04-07 00:17:10 -0400

    added bootstrap, made column layout, added rotten tomatoes and actor information, wrapped input in form tags

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,16 +1,20 @@
 $(document).ready(function(){
-	$("#btn").click(function(){
+	$("form").submit(function(e){
+		e.preventDefault();
 		var title = $("#query").val();
 		
 
 		$.ajax({
-            url: "http://www.omdbapi.com/?t="+title,
+            url: "http://www.omdbapi.com/?tomatoes=true&t="+title,
             type: 'GET',
             success: function(data) {
                if(data.Response != "False"){
-               	$("#movie-title").text(data.Title);
-               $("#plot").text(data.Plot);
-               $("#poster").html('<img src='+data.Poster+'">');
+               	$("#movie-title").text("Title: "+data.Title);
+               	$("#plot").text("Plot: "+data.Plot);
+               	$("#actors").text("Actors: "+data.Actors);
+               	$("#tomato-review").text("Rotten Tomatoes: "+data.tomatoConsensus);
+               	$(".poster").html('<img src='+data.Poster+'">');
+               	$(".tomato-meter").text("Tomato Meter: "+data.tomatoMeter);
                }
                else{
                	alert("no results")
